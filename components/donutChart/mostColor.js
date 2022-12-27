ef36c446960c046adfa91fd3460ce4c62916e466
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2022-12-27 13:38:50 +0700

    chart config and setting
    show all most color, all most size

--- components/donutChart/mostColor.js
+++ components/donutChart/mostColor.js
@@ -4,6 +4,11 @@ import { useState, useEffect } from "react"
 import dynamic from 'next/dynamic';
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
+//Font awesome classicon
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons"
+
 export default function MostColor() {
     //Initial variable
     const [error, setError] = useState(null);
@@ -15,10 +20,15 @@ export default function MostColor() {
     const data = Object.values(items);
 
     useEffect(() => {
-        fetch("https://customanalytic.leonardhors.site/api/product/mostcolor/15")
+        fetch("https://customanalytic.leonardhors.site/api/product/mostcolor/"+sessionStorage.getItem("ChartLimit_MostColor"))
         .then(res => res.json())
             .then(
             (result) => {
+                //Default config
+                if(sessionStorage.getItem("ChartLimit_MostColor") == null){
+                    sessionStorage.setItem("ChartLimit_MostColor", "10");
+                }
+
                 setIsLoaded(true);
                 setItems(result.data);
             },
@@ -57,6 +67,12 @@ export default function MostColor() {
         }
     };
 
+    //Chart filter and config
+    function setLimit(limit){
+        sessionStorage.setItem("ChartLimit_MostColor", limit);
+        location.reload();
+    }
+
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -65,6 +81,13 @@ export default function MostColor() {
         return (
             <div className='custom-tbody' style={{padding:"6px"}}>
                 <h6 className='text-white'>Most Produced Color</h6>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <FontAwesomeIcon icon={faEllipsisVertical}/></button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item"><input type="number" className='form-control' min="2" defaultValue={sessionStorage.getItem("ChartLimit_MostColor")} onBlur={(e)=> setLimit(e.target.value)}></input></a></li>
+                    </ul>
+                </div>
                 <div className="BalanceChart me-4">
                     <Chart
                         options={chart.options}
--- components/donutChart/mostSize.js
+++ components/donutChart/mostSize.js
@@ -4,23 +4,50 @@ import { useState, useEffect } from "react"
 import dynamic from 'next/dynamic';
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
+//Font awesome classicon
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faEllipsisVertical } from "@fortawesome/free-solid-svg-icons"
+
 export default function MostSize() {
     //Initial variable
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
+    const [items2, setItems2] = useState([]);
     var chart = [];
 
     //Converter
     const data = Object.values(items);
 
     useEffect(() => {
-        fetch("https://customanalytic.leonardhors.site/api/product/mostsize/10/Bikes")
+        fetch("https://customanalytic.leonardhors.site/api/product/mostsize/"+sessionStorage.getItem("ChartLimit_MostSize")+"/"+sessionStorage.getItem("ChartType_MostSize"))
+        .then(res => res.json())
+            .then(
+            (result) => {
+                //Default config
+                if(sessionStorage.getItem("ChartLimit_MostSize") == null || sessionStorage.getItem("ChartType_MostSize") == null){
+                    sessionStorage.setItem("ChartLimit_MostSize", "10");
+                    sessionStorage.setItem("ChartType_MostSize", "Bikes");
+                }
+
+                setIsLoaded(true);
+                setItems(result.data);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    },[])
+
+    useEffect(() => {
+        fetch("https://customanalytic.leonardhors.site/api/product/category")
         .then(res => res.json())
             .then(
             (result) => {
                 setIsLoaded(true);
-                setItems(result.data);
+                setItems2(result.data);
             },
             (error) => {
                 setIsLoaded(true);
@@ -57,6 +84,17 @@ export default function MostSize() {
         }
     };
 
+    //Chart filter and config
+    function setLimit(limit){
+        sessionStorage.setItem("ChartLimit_MostSize", limit);
+        location.reload();
+    }
+
+    function setCategory(type){
+        sessionStorage.setItem("ChartType_MostSize", type);
+        location.reload();
+    }
+
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -65,6 +103,27 @@ export default function MostSize() {
         return (
             <div className='custom-tbody' style={{padding:"6px"}}>
                 <h6 className='text-white'>Most Produced Size</h6>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <FontAwesomeIcon icon={faEllipsisVertical}/></button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        {
+                            //Category type filter
+                            items2.map((val, i, index) => {
+                                if(val.CategoryName == sessionStorage.getItem("ChartType_MostSize")){
+                                    return (
+                                        <li key={i}><a className="dropdown-item"><FontAwesomeIcon icon={faCheck} onClick={(e)=> setCategory(val.CategoryName)}/> {val.CategoryName}</a></li>
+                                    );
+                                } else {
+                                    return (
+                                        <li key={i}><a className="dropdown-item" onClick={(e)=> setCategory(val.CategoryName)}>{val.CategoryName}</a></li>
+                                    );
+                                }
+                            })
+                        }
+                        <li><a className="dropdown-item"><input type="number" className='form-control' min="2" defaultValue={sessionStorage.getItem("ChartLimit_MostSize")} onBlur={(e)=> setLimit(e.target.value)}></input></a></li>
+                    </ul>
+                </div>
                 <div className="BalanceChart me-4">
                     <Chart
                         options={chart.options}
